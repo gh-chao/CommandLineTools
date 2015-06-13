e39db4b17357403cb0f2f333a22ec4a908870bb4
leo <897798676@qq.com>
2015-06-13 18:30:30 +0800

    2333

--- bin/console.php
+++ bin/console.php
@@ -2,12 +2,13 @@
 <?php
 
 
+use DbDocCreator\DbDocCreatorCommand;
 use PHPExtensionDocCreator\PHPExtensionDocCreatorCommand;
 
 include dirname(__DIR__) . '/vendor/autoload.php';
 
 $console = new \Console\Application();
-$console->add(new \DbDocCreator\DbDocCreatorCommand());
+$console->add(new DbDocCreatorCommand());
 $console->add(new PHPExtensionDocCreatorCommand());
 
 $console->run();
